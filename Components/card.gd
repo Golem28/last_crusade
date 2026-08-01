5b39eebf0b490c1107e774edeeeb95afbc7b3f1e
Konrad Ebel <despair2400@proton.me>
2026-08-01 21:31:20 +0200

    Feature: Drop zones have a type, that determines card abilities

--- Components/card.gd
+++ Components/card.gd
@@ -18,8 +18,17 @@ func handle_valid_drop(drop_zone: DropZone) -> void:
 	super.handle_valid_drop(drop_zone)
 	self.custom_minimum_size = Vector2(120, 120)
 	self.set_size(Vector2(120, 120))
-	(get_node("Content/Front") as Control).visible = false
-	(get_node("Content/Flank") as Control).visible = false
+	
+	var drop_zone_type := drop_zone.get_drop_zone_type()
+	
+	if drop_zone_type != DropZone.ZONE_TYPE.FRONT:
+		(get_node("Content/Front") as Control).visible = false
+
+	if drop_zone_type != DropZone.ZONE_TYPE.FLANK:
+		(get_node("Content/Flank") as Control).visible = false
+
+	if drop_zone_type != DropZone.ZONE_TYPE.REAR:
+		(get_node("Content/Rear") as Control).visible = false
 
 func _setup_card_data() -> void:
 	var front_label = get_node("Content/Front/Label")
--- Components/drop_zone.gd
+++ Components/drop_zone.gd
@@ -2,6 +2,8 @@ extends ColorRect
 
 class_name DropZone
 
+enum ZONE_TYPE {FRONT, FLANK, REAR}
+
 @export var zone_size := Vector2(100, 140)
 
 func get_drop_rect() -> Rect2:
@@ -9,6 +11,10 @@ func get_drop_rect() -> Rect2:
 
 func get_card_anchor_position() -> Vector2:
 	return global_position
+
+func get_drop_zone_type() -> ZONE_TYPE:
+	var values := ZONE_TYPE.values()
+	return values[randi() % ZONE_TYPE.size()]
 
 func is_occupied() -> bool:
 	return get_child_count() > 0
